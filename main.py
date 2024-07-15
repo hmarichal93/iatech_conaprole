dcbb944932e5ed44246fdcc88ae6b530f5a2be82
henry <hmarichal93@gmail.com>
2024-07-15 13:56:01 -0300

    labelled fix

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def mannually_modify_predictions(dataset_dir, prediction_dirs, annotated_files="
     output_dir = Path(prediction_dirs).parent / "modified"
     output_dir.mkdir(parents=True, exist_ok=True)
     for json_path in json_files:
-        if json_path.name in pd_file["json_file"].values:
+        get_location_where_substring_appears = [json_path.name for annotated_path in list(pd_file["json_file"].values) if json_path.name in annotated_path]
+        is_json_path_already_annotated = len(get_location_where_substring_appears) > 0
+        if is_json_path_already_annotated:
             continue
 
         #######fix the json file to point to the correct image
@@ -27,11 +29,11 @@ def mannually_modify_predictions(dataset_dir, prediction_dirs, annotated_files="
             json.dump(json_content, json_file)
         #############################################################################################
         json_new_name = output_dir / (json_path.stem.replace(" ","_") + "_modified.json")
-        command = f"cp \"{str(json_path)}\" {str(json_new_name)}"
+        command = f"cp \"{str(json_path)}\" \"{str(json_new_name)}\""
         print(command)
         os.system(command)
         #command = (f"cd {str(output_dir)} && " +
-        command = f"labelme \"{str(json_path)}\""  + " --output " + str(json_new_name)
+        command = f"labelme \"{str(json_path)}\" --output \"{str(json_new_name)}\""
         print(command)
         os.system(command)
 
